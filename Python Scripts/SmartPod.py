a81420194ca11daab87becc3547e117c401caae9
FeliziusV <37139067+FeliziusV@users.noreply.github.com>
2020-01-09 18:50:38 +0100

    post image feature added

--- Python Scripts/SmartPod.py
+++ Python Scripts/SmartPod.py
@@ -1,35 +1,32 @@
 import serial
 import time
+import os
+import requests
+import json
+import ftplib
 from datetime import datetime
-import mysql.connector
-from mysql.connector import Error
 from picamera import PiCamera
 from time import sleep
 from flask import request
-import requests
-import json
+
 
 def postTemperature(temperature):
     payload = {"plant_id" : 1, "unit" : "celsius", "value" : str(temperature), "type" : "temperature"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
-    print(r.text)
     
 def postHumidityAir(humidity):
     payload = {"plant_id" : 1, "unit" : "percent", "value" : str(humidity), "type" : "humidity_air"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
-    print(r.text)
     
 def postHumiditySoil(humidity):
     payload = {"plant_id" : 1, "unit" : "percent", "value" : str(humidity), "type" : "humidity_soil"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
-    print(r.text)
 
-def writeToFile(text):
-    file = open("data_water.txt","a") 
-    file.write(text)
-    file.write("\n")
-    file.close()
-    
+def postImageName(name):
+    payload = {"plant_id" : 1, "value" : str(name)}
+    r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
+
+
 def getTime():
     dateTimeObj = datetime.now()
     dateTimeObj =dateTimeObj.strftime('%Y-%m-%d %H:%M')
@@ -46,34 +43,14 @@ def takePicture(plantid,date):
     camera.capture("Images/"+name+".jpg")
     camera.stop_preview()
     camera.close()
-
     
-    
-def insertVariblesIntoTable(date,plant_id, value):
-    try:
-        connection = mysql.connector.connect(host='localhost',
-                                             database='smartpod',
-                                             user='fot',
-                                             password='fot')
-
-        cursor = connection.cursor()
-        mySql_insert_query = """INSERT INTO humidity (date, plant_id, value) 
-                                VALUES (%s, %s, %s) """
-
-        recordTuple = (date, plant_id, value)
-        cursor.execute(mySql_insert_query, recordTuple)
-        connection.commit()
-        print("Record inserted successfully")
-
-    except mysql.connector.Error as error:
-        print("Failed to insert into MySQL table".format(error))
-
-    finally:
-        if (connection.is_connected()):
-            cursor.close()
-            connection.close()
-            print("MySQL connection is closed")
-            
+    session =ftplib.FTP('www12.world4you.com','ftp5166141_felix','felix12345')
+    file= open("Images/"+name+".jpg",'rb')
+    session.storbinary("STOR images/"+name+".jpg",file)
+    file.close()
+    session.quit()
+    print("image uploaded")
+    os.remove("Images/"+name+".jpg")
             
 ser = serial.Serial('/dev/ttyACM0', 9600)
 plantid=1;
@@ -85,23 +62,19 @@ while 1:
                 line =line.decode('utf-8')
                 timestamp=getTime()
                 values=line.split(" ")
-               
                 humidity_pot=int(values[0])/1023*100
                 humidity_pot=round(humidity_pot,2)
-                
                 humidity_air=float(values[1])
                 temperature=values[2];
                 print(timestamp+"- pot humidity: "+str(humidity_pot)+" air humidity: "+str(humidity_air)+" temperature: "+str(temperature))
                 postTemperature(str(temperature).rstrip());
                 postHumidityAir(str(humidity_air));
                 postHumiditySoil(str(humidity_pot));
-                #writeToFile(str(getTime())+" - "+line.decode('utf-8'))
-            
-                #insertVariblesIntoTable(timestamp,1,humidity_pot)
                 takePicture(plantid,timestamp)
                 time.sleep(300)
+                
             except SerialException:
-                print ("error ")
+                print ("Serial Error")
         
 
 
