883b3722b75f13a5cae92642e8ea58e32d7943fa
FeliziusV <37139067+FeliziusV@users.noreply.github.com>
2020-01-10 19:49:39 +0100

    config file added

---
+++ Python Scripts/SmartPodConfig.py
@@ -0,0 +1,7 @@
+general = {'serialId': '/dev/ttyACM1',
+         'plantId': '1',
+         'serverUrl': 'add something',
+         'ftpUrl': 'add something',
+         'ftpUser': 'add something',
+         'ftpPassword': 'add something'}
+
