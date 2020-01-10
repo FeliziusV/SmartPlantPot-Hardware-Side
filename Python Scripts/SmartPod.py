c2406d90945c22d4e849fc2211b47f7a20c320df
FeliziusV <37139067+FeliziusV@users.noreply.github.com>
2020-01-10 09:36:24 +0100

    comments added

--- Python Scripts/SmartPod.py
+++ Python Scripts/SmartPod.py
@@ -9,29 +9,32 @@ from picamera import PiCamera
 from time import sleep
 from flask import request
 
-
+#uploads temperature value to server
 def postTemperature(temperature):
     payload = {"plant_id" : 1, "unit" : "celsius", "value" : str(temperature), "type" : "temperature"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
-    
+
+#uploads air-humidity value to server
 def postHumidityAir(humidity):
     payload = {"plant_id" : 1, "unit" : "percent", "value" : str(humidity), "type" : "humidity_air"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
-    
+
+#uploads soil-humidity value to server
 def postHumiditySoil(humidity):
     payload = {"plant_id" : 1, "unit" : "percent", "value" : str(humidity), "type" : "humidity_soil"}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
 
+#uploads image name to server
 def postImageName(name):
     payload = {"plant_id" : 1, "value" : str(name)}
     r = requests.post('http://www.trabby.at/spp/api/measurement/create.php', json=payload)
 
-
+#returns current time
 def getTime():
     dateTimeObj = datetime.now()
     dateTimeObj =dateTimeObj.strftime('%Y-%m-%d %H:%M')
     return dateTimeObj
-
+#takes picture and uploads it to the server
 def takePicture(plantid,date):
     camera = PiCamera()
     camera.start_preview(fullscreen=False,window=(100,200,300,400))
@@ -51,7 +54,8 @@ def takePicture(plantid,date):
     session.quit()
     print("image uploaded")
     os.remove("Images/"+name+".jpg")
-            
+
+#-----------------main logic -----------------------
 ser = serial.Serial('/dev/ttyACM0', 9600)
 plantid=1;
 
@@ -66,7 +70,7 @@ while 1:
                 humidity_pot=round(humidity_pot,2)
                 humidity_air=float(values[1])
                 temperature=values[2];
-                print(timestamp+"- pot humidity: "+str(humidity_pot)+" air humidity: "+str(humidity_air)+" temperature: "+str(temperature))
+                print(timestamp+" - pot humidity: "+str(humidity_pot)+" air humidity: "+str(humidity_air)+" temperature: "+str(temperature))
                 postTemperature(str(temperature).rstrip());
                 postHumidityAir(str(humidity_air));
                 postHumiditySoil(str(humidity_pot));
