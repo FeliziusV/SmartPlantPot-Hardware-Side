49452acb9d3d01594ed3caaef130a4b6d665530d
FeliziusV <37139067+FeliziusV@users.noreply.github.com>
2020-01-21 12:37:38 +0100

    Update SmartPodConfig.py

--- Python Scripts/SmartPodConfig.py
+++ Python Scripts/SmartPodConfig.py
@@ -1,7 +1,11 @@
-general = {'serialId': '/dev/ttyACM1',
-         'plantId': '1',
-         'serverUrl': 'add something',
-         'ftpUrl': 'add something',
-         'ftpUser': 'add something',
-         'ftpPassword': 'add something'}
+#Configuration File for the SmartPod.py Script
+
+general = {'serialId': '/dev/ttyACM0',
+         'plantId': 'add here',
+         'apiCreateMeasurementUrl': 'add here',
+         'apiCreateImageUrl': 'add here',
+         'serverImageFolderUrl': 'add here',
+         'ftpServerUrl': 'add here',
+         'ftpServerUser': 'add here',
+         'ftpServerPassword': 'add here'}
 
